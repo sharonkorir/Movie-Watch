aaaec2f2f6be55fdbc33e7c11bab22e8ed29aa8f
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-10 09:46:44 +0300

    create movie model and test

--- app/views.py
+++ app/views.py
@@ -3,4 +3,16 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+
+    return render_template('index.html',title=title)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
