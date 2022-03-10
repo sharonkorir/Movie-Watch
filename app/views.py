446ca903146db2b0f68efc69c80da8218b39900c
sharonkorir <sharon.korir@student.moringaschool.com>
2022-03-10 12:15:06 +0300

    display movie objects

--- app/views.py
+++ app/views.py
@@ -1,18 +1,28 @@
 from flask import render_template
 from app import app
+from .request import get_movies,get_movie
+
+
 
 @app.route('/')
 def index():
-    
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+  # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to Movie watch'
+    # Getting movie
+    movie = get_movie
+    return render_template('index.html', title = title,popular = popular_movies)
 
-    return render_template('index.html',title=title)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
